5179f3be0b883299892a683fbd11f3e3bed379ec
Damien Detcherry <damien@captur.io>
2011-07-28 23:31:25 +0200

    text changes to get@captur.io mails

--- get_capturio_mail.py
+++ get_capturio_mail.py
@@ -12,9 +12,9 @@ class GetMailHandler():
 	
 	def sendResponse(self, typeOfResponse, attachments = None, label = None, mail = None):
 		if(typeOfResponse == "noAttachment"):
-			self.responseMail.subject = "We didn't find any ID image in your last email"
+			self.responseMail.subject = "We didn't find any image in your last email"
 			self.responseMail.body = """			
-We're sorry but we didn't find any ID image in your last email. Please double check and make sure you've got one ID image so that we can send you the associated contact information.
+We're sorry but we didn't find any image in your last email. Please double check and make sure you've got one image so that we can send you the associated contact information.
 			"""
 		elif(typeOfResponse == "tooManyAttachments"):
 			self.responseMail.subject = "Too many files in your last email. We process only one image per email"
@@ -32,19 +32,19 @@ We're sorry but we did not recognize the extension of your image. Please make su
 We're sorry but we don't know this image. Please retry with a picture with more light and/or precision.
 			"""
 		elif(typeOfResponse == "noVcardAssociated"):		
-			self.responseMail.subject = "The user we found did not associate any vcard"
+			self.responseMail.subject = "The user we found did not associate any associated contact information"
 			self.responseMail.body = """
-We're sorry but the user matching your image did not associate any vcard. We'll ping him so that he'll input this information!
+We're sorry but the user matching your image did not associate any contact information. We'll ping him so that he'll input this information!
 			"""	
 		elif(typeOfResponse == "infoNotConfirmed"):
 			self.responseMail.subject = "The user recognized did not confirm his information"
 			self.responseMail.body = """
-We found an image matching yours. Yet, the user found did not confirm his information. Tell him he has to confirm this visual first in order to share his vcard.
+We found an image matching yours. Yet, the user found did not confirm his information. Tell him he has to confirm this visual first in order to share his contact information.
 			"""
 		elif(typeOfResponse == "vcardAttached"):
-			self.responseMail.subject = "[Capturio rocks] Please find attached the vcard you requested"
+			self.responseMail.subject = "[Capturio rocks] Please find attached the contact information (.vcf) you requested"
 			self.responseMail.body = """
-User: %s (%s) has been recognized. Please find attached his vcard. Isn't it magical? Feel free to send us other images. 
+User: %s (%s) has been recognized. Please find attached his contact information (vcard .vcf). Isn't it magical? Feel free to send us other images. 
 			""" % (label, mail)
 			self.responseMail.attachments = attachments	
 		elif(typeOfResponse == "error"):
@@ -61,7 +61,7 @@ Ping us again, soon!
 
 Captur.io crew
 
-PS: If your vcard is not associated yet with one of your personal objects, just send both to post@captur.io. For more information, go to http://captur.io		
+PS: If your contact information (vcard .vcf) is not associated yet with one of your tshirts, just send both to post@captur.io. For more information, go to http://captur.io		
 		"""
 
 		self.responseMail.body = hello + self.responseMail.body + signature	
@@ -75,13 +75,13 @@ PS: If your vcard is not associated yet with one of your personal objects, just 
 		if(typeOfResponse == "justCapturedRequesterWithVcard"):
 			self.responseMail.subject = "[Vcard attached] You have been captured by another person on Captur.io"
 			self.responseMail.body = """
-The following person: %s (%s) captured you with Capturio! He just received your vcard because you have probably shown him your Capturio object! Please find also attached his own vcard.
+The following person: %s (%s) captured you with Capturio! He just received your contact information because you have probably shown him your tshirt! Please find also attached his own contact information.
 			""" % (label, mail)
 			self.responseMail.attachments = attachments		
 		elif(typeOfResponse == "justCapturedRequesterWithoutVcard"):
 			self.responseMail.subject = "You have been captured by another person on Captur.io"
 			self.responseMail.body = """
-The following person: %s (%s) captured you with Capturio! He just received your vcard because you have probably shown him your Capturio object!
+The following person: %s (%s) captured you with Capturio! He just received your contact information because you have probably shown him your tshirt!
 			""" % (label, mail)
 	
 		hello = """
